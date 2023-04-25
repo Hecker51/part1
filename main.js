f1f8201b153b7c22d3dc6008daddacb88012397d
Hecker51 <111092624+Hecker51@users.noreply.github.com>
2023-04-25 18:50:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-function preload(){}
-function draw(){}
-function setup(){}
+function preload()
+{
+
+}
+function draw()
+{
+    image(video, 0, 0, 300, 300)
+
+}
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+   
+  
+  }
+
+//💩💩//💩💩//💩💩//💩💩//💩💩//💩💩//💩💩//💩💩//💩💩//💩💩//💩💩//💩💩
 
 
 
 function a123(){
-    
+
+}
+
+function modelLoaded()
+{
+
+}
+
+function gotPoses()
+{
+
 }
